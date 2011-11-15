59d029494322f1dd1badcbe609745e4c9f182ec4
bac2g10 <bac2g10@ecs.soton.ac.uk>
2011-11-15 13:49:59 +0000

    Implemented myinit

--- bestfit/mymalloc.c
+++ bestfit/mymalloc.c
@@ -15,10 +15,19 @@
 int myinit(int *array, int size){
 	
 	//check that the array is large enough
+	if(size < MIN_ARRAY_SIZE){
+		return 0;
+	}
 
 	//point head to first linked list entry
+	array[0] = 1;
 
 	//set up first linked list entry
+	array[1] = size - 3; //1 Overhead for first linked list, 2 for each allocation
+	array[size-1] = size - 3; //Tail size;
+	array[2] = 1; //Next pointer
+	array[3] = 1; //Tail pointer
+	return 1;
 
 }
 
